40ed185cc07f79e6c79afa17405b1fb3101eeaef
Sandro Arpi <sandro.teste72@gmail.com>
2022-07-18 13:48:42 -0300

    Project created

---
+++ backend/src/test/java/com/arpitech/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpitech.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
